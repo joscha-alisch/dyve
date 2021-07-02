1c68b2f9af43f6430c5c9872b1f39085e57ec96e
Joscha Alisch <joscha.alisch@gmail.com>
2021-07-02 10:19:14 +0200

    Return reconcile error

---
+++ internal/provider/cloudfoundry/errors.go
@@ -0,0 +1,38 @@
+package cloudfoundry
+
+import (
+	"errors"
+	"fmt"
+)
+
+var errNotFound = errors.New("not found")
+
+type errReconcileFailed struct {
+	Err error
+	Job ReconcileJob
+}
+
+func (r *errReconcileFailed) Is(target error) bool {
+	if rFailed, ok := target.(*errReconcileFailed); ok {
+		return rFailed.Job.Type == r.Job.Type &&
+			rFailed.Job.Guid == r.Job.Guid && errors.Is(rFailed.Err, r.Err)
+	}
+	return false
+}
+
+func (r *errReconcileFailed) Unwrap() error {
+	return r.Err
+}
+
+func (r *errReconcileFailed) Error() string {
+	t := ""
+	switch r.Job.Type {
+	case ReconcileOrg:
+		t = "org"
+	case ReconcileApp:
+		t = "app"
+	case ReconcileSpace:
+		t = "space"
+	}
+	return fmt.Sprintf("%s reconcile failed for guid '%s': %s", t, r.Job.Guid, r.Err)
+}
