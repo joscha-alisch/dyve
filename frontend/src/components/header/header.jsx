264b60f0ce644b05345a8beb4736d2b81a9578b4
Joscha Alisch <joscha.alisch@gmail.com>
2021-10-05 21:10:12 +0200

    Add user box in header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,6 +1,14 @@
 import styles from "./header.module.sass"
+import {useUser} from "../../context/auth";
 
-const Header = () => <header className={styles.Header}>
-</header>;
+const Header = () => {
+    let user = useUser()
+    return <header className={styles.Header}>
+        <div className={styles.UserBox}>
+            <img className={styles.Avatar} alt="user avatar" src={user.picture}/>
+            <span className={styles.Username}>{user.name}</span>
+        </div>
+    </header>;
+}
 
 export default Header
