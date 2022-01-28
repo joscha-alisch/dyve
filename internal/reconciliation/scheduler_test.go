48713e4c19a292c4275a2f89c15c7edc7280716c
Joscha Alisch <joscha.alisch@gmail.com>
2022-01-28 11:40:07 +0100

    (#87) Fix data race in test

--- internal/reconciliation/scheduler_test.go
+++ internal/reconciliation/scheduler_test.go
@@ -1,31 +1,39 @@
 package reconciliation
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
 
 func TestScheduler(t *testing.T) {
-	r := &fakeReconciler{}
+	r := &fakeReconciler{
+		mux: &sync.Mutex{},
+	}
 	s := NewScheduler(r).(*scheduler)
 
 	_ = s.Run(1, 10*time.Millisecond)
 	time.Sleep(120 * time.Millisecond)
 	close(s.cancel)
 
+	r.mux.Lock()
 	if r.times < 10 || r.times > 15 {
 		t.Error("expected reconciler to have been called roughly 12 times")
 	}
+	r.mux.Unlock()
 }
 
 type fakeReconciler struct {
 	ok    bool
 	err   error
 	times int
+	mux   *sync.Mutex
 }
 
 func (f *fakeReconciler) Run() (bool, error) {
+	f.mux.Lock()
 	f.times++
+	f.mux.Unlock()
 	return f.ok, f.err
 }
 
